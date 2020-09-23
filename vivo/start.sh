26a5d07a1069dc2cb7e168d4f9b4ded05ff5ca92
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-09-23 12:13:18 -0400

    remove vivo from url path

--- vivo/start.sh
+++ vivo/start.sh
@@ -3,5 +3,7 @@ cd /usr/local/VIVO/VIVO-1.11.1
 mvn install -Dmaven.test.skip=true -s ../example-settings.xml
 cp /usr/local/VIVO/VIVO-1.11.1/runtime.properties /usr/local/VIVO/home/config/
 cp /usr/local/VIVO/VIVO-1.11.1/applicationSetup.n3 /usr/local/VIVO/home/config/
+cd /usr/local/tomcat/webapps
+ln -s vivo ROOT
 catalina.sh run
 
