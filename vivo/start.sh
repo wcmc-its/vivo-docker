d0221fdfd032769aec9034b3ffda3ff29381ca94
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-09-28 13:48:57 -0400

    put vivo in different folder

--- vivo/start.sh
+++ vivo/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /usr/local/VIVO
-mv /usr/local/Vitro/Vitro-1.11.1 /usr/local/VIVO
-mv Vitro-1.11.1 Vitro
+mv /usr/local/Vitro/Vitro-master /usr/local/VIVO
+mv Vitro-master Vitro
 cd /usr/local/VIVO/VIVO
 mvn clean install -Dmaven.test.skip=true -s ../example-settings.xml
 cp /usr/local/VIVO/VIVO/runtime.properties /usr/local/VIVO/home/config/
 cp /usr/local/VIVO/VIVO/applicationSetup.n3 /usr/local/VIVO/home/config/
 cd /usr/local/tomcat/webapps
-ln -s vivo ROOT
+ln -s /usr/local/VIVO/webapps/vivo ROOT
 catalina.sh run
 
