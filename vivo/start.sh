aabbdccb024fe87055ecd64aa6260d242c1e5a98
Sarbajit Dutta <szd2013@med.cornell.edu>
2020-11-23 10:58:35 -0500

    add catalina options

--- vivo/start.sh
+++ vivo/start.sh
@@ -8,5 +8,6 @@ cp /usr/local/VIVO/VIVO/runtime.properties /usr/local/VIVO/home/config/
 cp /usr/local/VIVO/VIVO/applicationSetup.n3 /usr/local/VIVO/home/config/
 cd /usr/local/tomcat/webapps
 ln -s /usr/local/VIVO/webapps/vivo ROOT
+echo "export CATALINA_OPTS="'"-Xms8192m -Xmx8192m -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/usr/local/VIVO"'"" > /usr/local/tomcat/bin/setenv.sh
 catalina.sh run
 
